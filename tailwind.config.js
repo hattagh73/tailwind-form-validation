8ee1d81d3034d71eedd63bcdc7b326c824f9b2f9
hattagh73 <>
2022-11-15 21:59:38 +0800

    add tailwind.config file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type { import('tailwindcss').Config } */
+
+tailwindConfig = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {},
+    plugins: [],
+}
+
+module.exports = tailwindConfig;
